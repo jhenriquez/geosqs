58425d1b1f950447c16229fd2c84fe37d8f04fb2
jhenriquez <julio.m.henriquez@gmail.com>
2014-10-18 09:27:19 -0400

    Setup Grunt for jasmine-node.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,17 @@ module.exports = function (g) {
 		pkg: g.file.readJSON('package.json'),
 		jshint: {
 			files: ['Gruntfile.js', 'app.js', 'SQSHandler.js','specs/**/**.js']
+		},
+		jasmine: {
+			all: ['specs/**/**.js']
 		}
 	});
 
 	g.loadNpmTasks('grunt-contrib-jshint');
+	g.loadNpmTasks('grunt-jasmine-node');
 
-	g.registerTask('test', ['jshint']);
+	g.registerTask('jasmine', ['jasmine_node']);
+	g.registerTask('lint', ['jshint']);
+
+	g.registerTask('default',['lint', 'jasmine']);
 };
