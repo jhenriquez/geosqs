26b04873b79a29b8daa9a0c6f87b058ce051f5d1
jhenriquez <julio.m.henriquez@gmail.com>
2014-10-18 10:14:06 -0400

    Fixed jasmine node settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,16 @@ module.exports = function (g) {
 		jshint: {
 			files: ['Gruntfile.js', 'app.js', 'SQSHandler.js','specs/**/**.js']
 		},
-		jasmine: {
-			all: ['specs/**/**.js']
+		jasmine_node: {
+			all: ['specs/']
 		}
 	});
 
 	g.loadNpmTasks('grunt-contrib-jshint');
 	g.loadNpmTasks('grunt-jasmine-node');
 
-	g.registerTask('jasmine', ['jasmine_node']);
+	g.registerTask('test', ['jasmine_node']);
 	g.registerTask('lint', ['jshint']);
 
-	g.registerTask('default',['lint', 'jasmine']);
+	g.registerTask('default',['lint', 'test']);
 };
